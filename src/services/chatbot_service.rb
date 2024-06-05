235d92ca93e25a030aa2656cc0bc4ae4a0a79c3a
Rusydy <rusydymuhiddin@gmail.com>
2024-06-05 23:26:38 +0700

    chore: Refactor ChatbotService to improve message handling
    The code changes in `ChatbotService` refactor the message handling logic to improve the flow and organization. It introduces new methods for sending greeting, instruction, registration, and error messages. The changes also remove unused code and update the WhatsApp API version in `.env.example`.

--- src/services/chatbot_service.rb
+++ src/services/chatbot_service.rb
@@ -12,15 +12,17 @@ class ChatbotService
       }
     end
 
-    case body_type
-    when 'text'
+    if body_type == 'text'
       return process_text_message(body)
+    elsif body_type == 'interactive'
+      return process_interactive_message(body)
     else
       return {
-        'status_code' => HTTPStatus::NoContent,
+        'status_code' => HTTPStatus::BadRequest,
         'message' => 'Invalid message type'
       }
     end
+
   end
 
   private
@@ -39,17 +41,56 @@ class ChatbotService
       }
     end
 
-    response = WhatsappService.send_text_message(profile_name, wa_id, text)
-    unless response['status_code'] == HTTPStatus::Ok
-      return {
-        'status_code' => response['status_code'],
-        'message' => response['message']
-      }
+    # TODO: Implement this later
+    # receiver = {
+    #   'profile_name' => profile_name,
+    #   'wa_id' => wa_id
+    # }.freeze
+
+    # flow for the text message
+    if text.downcase == 'hello' || text.downcase == 'hi' || text.downcase == 'hey'
+      send_greeting_message(profile_name, wa_id)
+      send_instruction_message(profile_name, wa_id)
+    elsif text == '1'
+      send_registration_message(profile_name, wa_id)
+    else
+      send_error_message(profile_name, wa_id)
     end
 
     {
       'status_code' => HTTPStatus::Ok,
       'message' => response['message']
     }
+  end
+
+  def self.send_greeting_message(profile_name, wa_id)
+    text = "Hello #{profile_name}!"
+    WhatsappService.send_text_message(profile_name, wa_id, text)
+
+    puts "Sent greeting message to #{profile_name} (#{wa_id}): #{text}"
+  end
+
+  def self.send_instruction_message(profile_name, wa_id)
+    text = "Please type 'help' for a list of available commands."
+    text += "\nType 1 for the registration."
+
+    WhatsappService.send_text_message(profile_name, wa_id, text)
+
+    puts "Sent instruction message to #{profile_name} (#{wa_id}): #{text}"
+  end
+
+  def self.send_registration_message(profile_name, wa_id)
+    # TODO: implement Whatsapp FLOW for registration
+    # send a registration form using flow
+  end
+
+  def self.send_error_message(profile_name, wa_id)
+    text = "Sorry, I don't understand what you mean."
+    text += "\nHere is the instruction:"
+    text += "\nType 1 for the registration."
+
+    WhatsappService.send_text_message(profile_name, wa_id, text)
+
+    puts "Sent error message to #{profile_name} (#{wa_id}): #{text}"
   end
 end
